6caeb1e5d7155252007a2754e28d9a5c29362df0
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-06-05 21:37:20 -0700

    adding the functions index JS step 5

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+npc.walkEast(500, () => {
+    npc.walkNorth(1400, () => {
+        npc.walkEast(1200, () => {
+            npc.walkSouth(300, () => )                                                   
+        })                                                   
+    })
+                                                       
+})
+
 
 // Create the inventory
 const inventory = newInventory()
